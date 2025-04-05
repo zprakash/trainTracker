8d557ce58badb96e5871e80f0dd0f3d75aec7b28
zprakash <prac303@gmail.com>
2025-04-05 19:27:54 +0300

    Update train info fetch time

--- src/component/TrainMap.vue
+++ src/component/TrainMap.vue
@@ -210,7 +210,7 @@ export default defineComponent({
 
          fetchDetailsInterval = setInterval(() => {
                fetchTrainDetailsPeriodically();
-           }, 60000); 
+           }, 45000); 
            
        });
 
